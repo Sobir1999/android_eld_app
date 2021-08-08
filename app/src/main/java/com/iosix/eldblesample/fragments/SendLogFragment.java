6fa86e5ebda3124133b130e8f869a95f779940e2
Sobir1999 <mustafoyevsobir0898@gmail.com>
2021-08-08 09:48:35 +0500

    send reports commit

--- app/src/main/java/com/iosix/eldblesample/fragments/SendLogFragment.java
+++ app/src/main/java/com/iosix/eldblesample/fragments/SendLogFragment.java
@@ -1,21 +1,25 @@
 package com.iosix.eldblesample.fragments;
 
 import android.content.Context;
-import android.graphics.PixelFormat;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import android.print.PrintAttributes;
-import android.print.pdf.PrintedPdfDocument;
 import android.view.LayoutInflater;
-import android.view.PixelCopy;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import android.widget.LinearLayout;
 import com.gkemon.XMLtoPDF.PdfGenerator;
 import com.gkemon.XMLtoPDF.PdfGeneratorListener;
 import com.gkemon.XMLtoPDF.model.FailureResponse;
@@ -26,7 +30,9 @@ import com.iosix.eldblesample.R;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class SendLogFragment extends Fragment {
 
@@ -35,6 +41,10 @@ public class SendLogFragment extends Fragment {
     Button button;
     Context context;
     View content;
+    Bitmap bitmap;
+    LinearLayout scrollView;
+    String email;
+    LinearLayout xmlToPdf;
 
     public SendLogFragment() {
         // Required empty public constructor
@@ -58,6 +68,10 @@ public class SendLogFragment extends Fragment {
         content = inflater.inflate(R.layout.fragment_begin_inspection,null);
         context = container.getContext();
         loadViews(view);
+
+        requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
         return view;
     }
 
@@ -66,6 +80,10 @@ public class SendLogFragment extends Fragment {
         img = view.findViewById(R.id.idImageBack);
         textInputEditText = view.findViewById(R.id.idSendLogEdittext);
         button = view.findViewById(R.id.idSendLogs);
+        scrollView = view.findViewById(R.id.idContainerSendLod);
+        email = view.findViewById(R.id.idSendLogEdittext).toString();
+        xmlToPdf = content.findViewById(R.id.idPrintView);
+
 
         img.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,68 +96,91 @@ public class SendLogFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                PdfGenerator.getBuilder()
-                        .setContext(context)
-                        .fromViewSource()
-                        .fromView(content)
-                        .setPageSize(PdfGenerator.PageSize.A4)
-                        .setFileName("Log Reports")
-                        .setFolderName("FastLogz/")
-                        .openPDFafterGeneration(true)
-                        .build(new PdfGeneratorListener() {
-                            @Override
-                            public void showLog(String log) {
-                                super.showLog(log);
-                            }
-
-                            @Override
-                            public void onStartPDFGeneration() {
-
-                            }
-
-                            @Override
-                            public void onFinishPDFGeneration() {
-
-                            }
-
-                            @Override
-                            public void onSuccess(SuccessResponse response) {
-                                super.onSuccess(response);
-                            }
-
-                            @Override
-                            public void onFailure(FailureResponse failureResponse) {
-                                super.onFailure(failureResponse);
-                            }
-                        });
-
-//                PrintAttributes printAttrs = new PrintAttributes.Builder().
-//                        setColorMode(PrintAttributes.COLOR_MODE_MONOCHROME).
-//                        setMediaSize(PrintAttributes.MediaSize.ISO_C7).
-//                        setResolution(new PrintAttributes.Resolution("zooey", Context.PRINT_SERVICE, 500, 500)).
-//                        setMinMargins(PrintAttributes.Margins.NO_MARGINS).
-//                        build();
+//                PdfGenerator.getBuilder()
+//                        .setContext(context)
+//                        .fromViewSource()
+//                        .fromView(content)
+//                        .setPageSize(PdfGenerator.PageSize.A4)
+//                        .setFileName("Log Reports")
+//                        .setFolderName("FastLogz/")
+//                        .openPDFafterGeneration(true)
+//                        .build(new PdfGeneratorListener() {
+//                            @Override
+//                            public void showLog(String log) {
+//                                super.showLog(log);
+//                            }
 //
-//                PdfDocument document = new PrintedPdfDocument(context, printAttrs);
-//                PdfDocument.PageInfo pageInfo = new  PdfDocument.PageInfo.Builder(content.getWidth(), content.getHeight(), 1).create();
-//                PdfDocument.Page page = document.startPage(pageInfo);
-//                content.draw(page.getCanvas());
-//                document.finishPage(page);
+//                            @Override
+//                            public void onStartPDFGeneration() {
 //
-//                try {
-//                    File pdfDirPath = new File(context.getFilesDir(), "pdfs");
-//                    pdfDirPath.mkdirs();
-//                    File file = new File(pdfDirPath, "Log Reports.pdf");
-//                    OutputStream os = new FileOutputStream(file);
-//                    document.writeTo(os);
-//                    document.close();
-//                    os.close();
-//                } catch (IOException e) {
-//                    throw new RuntimeException("Error generating file", e);
-//                }
+//                            }
+//
+//                            @Override
+//                            public void onFinishPDFGeneration() {
+//
+//                            }
+//
+//                            @Override
+//                            public void onSuccess(SuccessResponse response) {
+//                                super.onSuccess(response);
+//                            }
+//
+//                            @Override
+//                            public void onFailure(FailureResponse failureResponse) {
+//                                super.onFailure(failureResponse);
+//                            }
+//                        });
+
+                bitmap = Bitmap.createBitmap(view.getWidth(),view.getHeight(),Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                Drawable drawable = view.getBackground();
+                if (drawable != null){
+                    drawable.draw(canvas);
+                }else {
+                    canvas.drawColor(Color.WHITE);
+                    drawable.draw(canvas);
+                }
+
+                PdfDocument pdfDocument = new PdfDocument();
+                PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo
+                        .Builder(960,1280,1)
+                        .create();
+
+                PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+                page.getCanvas().drawBitmap(bitmap,0,0,null);
+                pdfDocument.finishPage(page);
+
+                String pdfFile = "Fastlogz/" + "Log Reports.pdf";
+                File myPdfFile = new File(pdfFile);
+
+                try {
+                    pdfDocument.writeTo(new FileOutputStream(myPdfFile));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                pdfDocument.close();
+
+                sendPdftoEmail(email);
 
             }
         });
 
     }
+
+
+
+    private void sendPdftoEmail(String eEmail){
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL,eEmail);
+
+        File file = new File("/sdcard/"+"FastLogz"+"/"+"Log Reports.pdf");
+
+        Uri URI = Uri.fromFile(file);
+        emailIntent.setDataAndType(URI,"application/pdf");
+        emailIntent.putExtra(Intent.EXTRA_STREAM,URI);
+        context.startActivity(Intent.createChooser(emailIntent,"Send email..."));
+    }
+
 }
