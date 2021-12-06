f5517bebb09a14f6501799d7fd1f11157bc4127b
Sobir1999 <mustafoyevsobir0898@gmail.com>
2021-12-06 09:13:34 +0500

    eld commit

---
+++ app/src/main/java/com/iosix/eldblesample/fragments/Config.java
@@ -0,0 +1,7 @@
+package com.iosix.eldblesample.fragments;
+
+
+public class Config {
+    public static final String EMAIL ="bmbmuhiddinova@gmail.com"; //your-gmail-username
+    public static final String PASSWORD ="Bmb197174"; //your-gmail-password
+}
